059558bb992ddc461cf3f1bf6be0116a7f614216
Dominik Mlynka <dominik.mlynka@gmail.com>
2021-10-19 21:04:09 +0200

    restructured project to use generic approach and interface defining basic stages of controlling k8s resource

---
+++ MonitoringController/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using k8s;
+using k8s.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringController
+{
+    public abstract class BaseController<T> : IController<T> where T : IKubernetesObject<V1ObjectMeta> 
+    {
+        protected IKubernetes client;
+
+        protected BaseController(IKubernetes client)
+        {
+            this.client = client;
+        }
+
+        public abstract void InitMonitoring();
+
+        public abstract void CheckAndUpdate();
+
+        public abstract void DeinitMonitoring();
+
+        public abstract void EventHandler(WatchEventType eventType, T resource);
+    }
+}
---
+++ MonitoringController/DeploymentsController.cs
@@ -0,0 +1,35 @@
+﻿using k8s;
+using k8s.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringController
+{
+    public class DeploymentsController : BaseController<V1Deployment>
+    {
+        public DeploymentsController(IKubernetes client) : base(client) { }
+
+        public override void EventHandler(WatchEventType eventType, V1Deployment resource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CheckAndUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeinitMonitoring()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InitMonitoring()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
